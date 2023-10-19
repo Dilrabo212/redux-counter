0af60cf2dd319a5dcb2b1197f22c605a5ff7d39e
dilrabo <dilraboxujamuratova@gmail.com>
2023-10-19 15:31:13 +0500

    finish easy counter in redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import {actions} from './store'
 
 function App() {
+
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch( actions.increment() );
+  }
+  const decrement = () => {
+    dispatch( actions.decrement());
+  }
+  const reset = () => {
+    dispatch(actions.reset());
+  }
+  const addBy = () => {
+    dispatch(actions.addBy(10));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="AppWrap">
+      <h1 className="AppCounter">Counter: {counter}</h1>
+    
+    <div className="AppButtons">
+    <button className="AppButtonIncriment" onClick={increment}>Increment</button>
+      <button className="AppButtonDecrement" onClick={decrement}>Decrement</button>
+      <button className="AppButtonReset" onClick={reset}>Reset</button>
+      <button className="AppButtonAddBy" onClick={addBy}>Add by 10</button>
+     </div>
     </div>
+
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,26 @@
+import { configureStore,combineReducers, createSlice } from '@reduxjs/toolkit';
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: { counter:0 },
+    reducers: {
+        increment: (state, action) => {
+            state.counter += 1;
+        },
+        decrement: (state, action) => {
+            state.counter -= 1;
+        },
+        reset: (state, action) => {
+            state.counter = 0;
+        },
+        addBy:(state, action) => {
+            state.counter += 10;
+        }
+    }
+})
+export  const  actions = counterSlice.actions;
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+export default store;
+
